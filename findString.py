989129edc64a9fc0d6f593a2c95548da3356a7be
Arpan Maity <33843098+binary-greens@users.noreply.github.com>
2020-05-11 09:12:21 +0530

    Create findString.py

---
+++ findString.py
@@ -0,0 +1,19 @@
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)):
+        if string[i] == sub_string[0]:
+            for j in range(len(sub_string)):
+                if (i+j) < len(string):
+                    if string[i+j] != sub_string[j]:
+                        break
+            else:
+                if (i+j)<len(string):
+                    count+=1
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
